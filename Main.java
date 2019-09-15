cf9d44e47a4aea562eadd0e2bb7132172ed5d10f
usernameslx <grisha55000@gmail.com>
2019-09-15 23:29:07 +0500

    first home work

---
+++ Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    int a, b;
+	    a = Integer.parseInt(sc.next());
+	    b = Integer.parseInt(sc.next());
+	    System.out.print(a + b);
+    }
+}
